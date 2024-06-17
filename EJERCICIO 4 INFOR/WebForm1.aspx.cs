f1e6227a3e498a5402fde9d9efc5e6611fd2cbe7
Leonel Chauca <71529078+LeonelChauca@users.noreply.github.com>
2024-06-16 22:28:54 -0400

    terminado

---
+++ EJERCICIO 4 INFOR/WebForm1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EJERCICIO_4_INFOR
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string nombre = Request.Params["nombre"].ToString();
+            string edad = Request.Params["apellido"].ToString();
+            Response.Write("<h1> Nombre:  " + nombre + "</h1>");
+            Response.Write("<h1> Edad:   " + edad + "</h1> ");
+        }
+    }
+}
